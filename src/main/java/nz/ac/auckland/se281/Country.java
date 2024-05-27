2ff48fffa15d140c5706d021c741a4dba6aa488d
Toshiro Mendoza <122713720+InZaneManInAZuite@users.noreply.github.com>
2024-05-27 20:53:38 +1200

    Populated class country to act as node for map engine data structure

--- src/main/java/nz/ac/auckland/se281/Country.java
+++ src/main/java/nz/ac/auckland/se281/Country.java
@@ -1,8 +1,47 @@
 package nz.ac.auckland.se281;
 
-
 public class Country {
-  private String id;
   private String name;
-  private int taxRate;
+  private String continent;
+  private int taxFee;
+
+  public Country(String name, String continent, int taxFee) {
+    this.name = name;
+    this.continent = continent;
+    this.taxFee = taxFee;
+  }
+
+  @Override
+  public String toString() {
+    return "Country{"
+        + "name='"
+        + name
+        + '\''
+        + ", continent='"
+        + continent
+        + '\''
+        + ", taxFee="
+        + taxFee
+        + '}';
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    Country other = (Country) obj;
+    if (name == null) {
+      if (other.name != null) return false;
+    } else if (!name.equals(other.name)) return false;
+    return true;
+  }
 }
--- src/main/java/nz/ac/auckland/se281/MapEngine.java
+++ src/main/java/nz/ac/auckland/se281/MapEngine.java
@@ -1,12 +1,16 @@
 package nz.ac.auckland.se281;
 
-import java.util.List;
+import java.util.*;
 
 /** This class is the main entry point. */
 public class MapEngine {
 
+  private Map<Country, List<Country>> adjCountry;
+
   public MapEngine() {
     // add other code here if you want
+    this.adjCountry = new HashMap<>();
+
     loadMap(); // keep this mehtod invocation
   }
 
@@ -15,6 +19,18 @@ public class MapEngine {
     List<String> countries = Utils.readCountries();
     List<String> adjacencies = Utils.readAdjacencies();
     // add code here to create your data structures
+
+    for (String countryData : countries) {
+      // add code here
+      String[] CountrySplit = countryData.split(",");
+      String name = CountrySplit[0];
+      String continent = CountrySplit[1];
+      int taxFee = Integer.parseInt(CountrySplit[2]);
+      Country country = new Country(name, continent, taxFee);
+
+      adjCountry.putIfAbsent(country, new ArrayList<>());
+      System.out.println(country.toString());
+    }
   }
 
   /** this method is invoked when the user run the command info-country. */
